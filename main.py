6f813da15e9026c323a2c241cda31146bef89336
HeigoTasuja <heigo.tasuja@gmail.com>
2025-01-08 15:22:46 +0200

    some functions

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+
+
 def main():
-
-
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
+
+
+def count_words(book):
+    words = book.split()
+    count = len(words)
+    print(count)
+
+
+def count_characters(book):
+    book_lower = book.lower()
+    char_count = {}
+
+    for char in book_lower:
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+    return char_count
 
 
 if __name__ == "__main__":
-    main()
-
+    book = main()
+    print(count_characters(book))
